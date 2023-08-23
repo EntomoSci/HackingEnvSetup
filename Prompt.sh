bbb0fc2fd9696d87df467f7945008b856800d38c
Pascal Salvador <70287304+typ1st@users.noreply.github.com>
2023-08-23 03:36:15 -0400

    Remove individual prompt setup script

--- Prompt.sh
+++
@@ -1,3 +0,0 @@
-echo "Updating PS1 environment variable (prompt) & adding changes to ~/.bashrc..."
-echo 'export PS1="-[\[$(tput sgr0)\]\[\033[38;5;10m\]\d\[$(tput sgr0)\]-\[$(tput sgr0)\]\[\033[38;5;10m\]\t\[$(tput sgr0)\]]-[\[$(tput sgr0)\]\[\033[38;5;214m\]\u\[$(tput sgr0)\]@\[$(tput sgr0)\]\[\033[38;5;196m\]\h\[$(tput sgr0)\]]-\n-[\[$(tput sgr0)\]\[\033[38;5;33m\]\w\[$(tput sgr0)\]]\\$ \[$(tput sgr0)\]"' >> ~/.bashrc
-echo "Done!"
