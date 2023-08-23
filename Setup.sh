07129278f43fa77b8b3dc3ab3143408e4134b990
Pascal Salvador <70287304+typ1st@users.noreply.github.com>
2023-08-23 11:12:22 -0400

    Update prompt structure

--- Setup.sh
+++ Setup.sh
@@ -2,7 +2,7 @@
 
 # Setting up the prompt.
 echo "Updating PS1 environment variable (prompt) & adding changes to ~/.bashrc..."
-echo 'export PS1="-[\[$(tput sgr0)\]\[\033[38;5;10m\]\d\[$(tput sgr0)\]-\[$(tput sgr0)\]\[\033[38;5;10m\]\t\[$(tput sgr0)\]]-[\[$(tput sgr0)\]\[\033[38;5;214m\]\u\[$(tput sgr0)\]@\[$(tput sgr0)\]\[\033[38;5;196m\]\h\[$(tput sgr0)\]]-\n-[\[$(tput sgr0)\]\[\033[38;5;33m\]\w\[$(tput sgr0)\]]\\$ \[$(tput sgr0)\]"' >> ~/.bashrc
+echo 'export PS1="[\[$(tput sgr0)\]\[\033[38;5;10m\]\d\[$(tput sgr0)\]-\[$(tput sgr0)\]\[\033[38;5;10m\]\t\[$(tput sgr0)\]]-[\[$(tput sgr0)\]\[\033[38;5;214m\]\u\[$(tput sgr0)\]@\[$(tput sgr0)\]\[\033[38;5;196m\]\h\[$(tput sgr0)\]]-[\[$(tput sgr0)\]\[\033[38;5;33m\]\w\[$(tput sgr0)\]]\n\\$ \[$(tput sgr0)\]"' >> ~/.bashrc
 source ~/.bashrc
 echo "Done!"
 
