52db4287d76a470b7ba494043ae834bf154a77ea
Pascal Salvador <70287304+typ1st@users.noreply.github.com>
2023-08-23 10:50:53 -0400

    Add installs for esential tools to setup script

--- Setup.sh
+++ Setup.sh
@@ -5,3 +5,13 @@ echo "Updating PS1 environment variable (prompt) & adding changes to ~/.bashrc..
 echo 'export PS1="-[\[$(tput sgr0)\]\[\033[38;5;10m\]\d\[$(tput sgr0)\]-\[$(tput sgr0)\]\[\033[38;5;10m\]\t\[$(tput sgr0)\]]-[\[$(tput sgr0)\]\[\033[38;5;214m\]\u\[$(tput sgr0)\]@\[$(tput sgr0)\]\[\033[38;5;196m\]\h\[$(tput sgr0)\]]-\n-[\[$(tput sgr0)\]\[\033[38;5;33m\]\w\[$(tput sgr0)\]]\\$ \[$(tput sgr0)\]"' >> ~/.bashrc
 source ~/.bashrc
 echo "Done!"
+
+# Installing esential tools.
+sudo apt update
+toolsURL="https://raw.githubusercontent.com/typ1st/HackingEnvSetup/main/Tools.list"
+tools=$(curl -s $toolsURL)
+echo "Installing esential tools..."
+for tool in $tools
+  do sudo apt install $tool -y
+done
+echo "Esential tools successfully installed!"
