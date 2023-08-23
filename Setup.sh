4a8af9fb14c16157433737c671fb736ab69ea81e
Pascal Salvador <70287304+typ1st@users.noreply.github.com>
2023-08-23 03:31:22 -0400

    Add shebang & hidding curl output in setup script

--- Setup.sh
+++ Setup.sh
@@ -1,4 +1,5 @@
+#!/bin/bash
 # Setting up the prompt.
 echo "Remote execution of prompt customization script..."
-curl https://raw.githubusercontent.com/typ1st/HackingEnvSetup/main/Prompt.sh | bash
+curl -s https://raw.githubusercontent.com/typ1st/HackingEnvSetup/main/Prompt.sh | bash
 source ~/.bashrc
