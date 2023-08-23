11a45a6659036234ddc6655fe359337f869314a2
Pascal Salvador <70287304+typ1st@users.noreply.github.com>
2023-08-23 11:24:32 -0400

    Add backup of ~/.bashrc before changing it

--- Setup.sh
+++ Setup.sh
@@ -3,6 +3,8 @@
 sudo apt update
 
 # Setting up the prompt.
+echo "Making backup of ~/.bashrc..."
+cp ~/.bashrc ~/.bashrc.bak
 echo "Updating PS1 environment variable (prompt) & adding changes to ~/.bashrc..."
 echo 'export PS1="[\[$(tput sgr0)\]\[\033[38;5;10m\]\d\[$(tput sgr0)\]-\[$(tput sgr0)\]\[\033[38;5;10m\]\t\[$(tput sgr0)\]]-[\[$(tput sgr0)\]\[\033[38;5;214m\]\u\[$(tput sgr0)\]@\[$(tput sgr0)\]\[\033[38;5;196m\]\h\[$(tput sgr0)\]]-[\[$(tput sgr0)\]\[\033[38;5;33m\]\w\[$(tput sgr0)\]]\n\\$ \[$(tput sgr0)\]"' >> ~/.bashrc
 source ~/.bashrc
