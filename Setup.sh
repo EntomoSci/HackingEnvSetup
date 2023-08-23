8c5acae6dc8185709cfaa66420ffab4562bb173d
Pascal Salvador <70287304+typ1st@users.noreply.github.com>
2023-08-23 02:45:42 -0400

    Add entry point script to orchestrate full setup

---
+++ Setup.sh
@@ -0,0 +1,4 @@
+# Setting up the prompt.
+echo "Remote execution of prompt customization script..."
+curl https://raw.githubusercontent.com/typ1st/HackingEnvSetup/main/Prompt.sh | bash
+source ~/.bashrc
