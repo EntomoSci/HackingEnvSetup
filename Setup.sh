be2ecdea80729a78fc14ccf0d54f4dfff8dbb99a
Pascal Salvador <70287304+typ1st@users.noreply.github.com>
2023-08-23 11:15:17 -0400

    Change order of user prompting for convenience

--- Setup.sh
+++ Setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt update
+
 # Setting up the prompt.
 echo "Updating PS1 environment variable (prompt) & adding changes to ~/.bashrc..."
 echo 'export PS1="[\[$(tput sgr0)\]\[\033[38;5;10m\]\d\[$(tput sgr0)\]-\[$(tput sgr0)\]\[\033[38;5;10m\]\t\[$(tput sgr0)\]]-[\[$(tput sgr0)\]\[\033[38;5;214m\]\u\[$(tput sgr0)\]@\[$(tput sgr0)\]\[\033[38;5;196m\]\h\[$(tput sgr0)\]]-[\[$(tput sgr0)\]\[\033[38;5;33m\]\w\[$(tput sgr0)\]]\n\\$ \[$(tput sgr0)\]"' >> ~/.bashrc
@@ -7,7 +9,6 @@ source ~/.bashrc
 echo "Done!"
 
 # Installing esential tools.
-sudo apt update
 toolsURL="https://raw.githubusercontent.com/typ1st/HackingEnvSetup/main/Tools.list"
 tools=$(curl -s $toolsURL)
 echo "Installing esential tools..."
