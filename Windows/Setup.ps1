d07c6220f7c703dc8a895e0977a0bf51265dc64a
Pascal Salvador <70287304+typ1st@users.noreply.github.com>
2023-08-24 12:01:21 -0400

    Add Windows environment setup script with chocolatey installation cmdlets

---
+++ Windows/Setup.ps1
@@ -0,0 +1,10 @@
+Write-Host "Beginning Windows environment setup"
+
+# NOTE: The cmdlet suite used to execute the Chocolatey install script is a slightly modified version of the official copy-paste command
+# in their website (https://chocolatey.org/install#individual), using 'Invoke-WebRequest' (iwr) instead of System.Net.WebClient whose
+# directly usage isn't recommended by Microsoft (https://learn.microsoft.com/en-us/dotnet/api/system.net.webclient?view=net-7.0#remark).
+# I choosen an user-friendly interface of higher level for a simple process here.
+Write-Host "Installing Chocolatey package manager from official installation script..."
+Set-ExecutionPolicy Bypass -Scope Process -Force && `
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072 && `
+(iwr -Uri 'https://community.chocolatey.org/install.ps1').Content | iex
