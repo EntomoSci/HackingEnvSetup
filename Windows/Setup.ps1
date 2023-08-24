271baa50edd1b09aabd4fafd1164ee2917b26ed7
Pascal Salvador <70287304+typ1st@users.noreply.github.com>
2023-08-24 13:34:16 -0400

    Clean Windows setup script & confirm tool installation

--- Windows/Setup.ps1
+++ Windows/Setup.ps1
@@ -4,6 +4,7 @@ Write-Host "Beginning Windows environment setup"
 # in their website (https://chocolatey.org/install#individual), using 'Invoke-WebRequest' (iwr) instead of System.Net.WebClient whose
 # directly usage isn't recommended by Microsoft (https://learn.microsoft.com/en-us/dotnet/api/system.net.webclient?view=net-7.0#remark).
 # I choosen an user-friendly interface of higher level for a simple process here.
+
 Write-Host "Installing Chocolatey package manager from official installation script..."
 Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
@@ -14,7 +15,8 @@ choco upgrade chocolatey -y
 
 # NOTE: Instead of iterating over the tools line by line, they are converted to an array using
 # Windows/Linux/Unix-like new lines characters as the separators to call choco one time.
-Write-Host "Installing essential tools..."
+
+Write-Host "Installing essential tools from remote list..."
 $toolsURL = "https://raw.githubusercontent.com/typ1st/HackingEnvSetup/main/Windows/Tools.txt"
 $tools = (iwr -Uri $toolsURL).Content.Trim() -split '\n|`r`n|`n'
-choco install $tools
+choco install $tools -y
