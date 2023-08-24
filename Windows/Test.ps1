d12df50d67c01ecc44d5f6c96dc23696023934b6
Pascal Salvador <70287304+typ1st@users.noreply.github.com>
2023-08-24 10:34:49 -0400

    Add powershell script to test web operations to gather scripts

---
+++ Windows/Test.ps1
@@ -0,0 +1,4 @@
+Write-Host "Hello, friend."
+Write-Host "You're gathering a Powershell script hosted in Github."
+Write-Host "If you're reading these messages it means that your web operations to get remote scripts work."
+Write-Host "Good look and keep going."
